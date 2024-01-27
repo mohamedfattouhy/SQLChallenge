3896dfe82ff5be14618d040254308aa98193a935
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2024-01-27 21:43:58 +0100

    Add config folder

---
+++ configuration/config.py
@@ -0,0 +1,8 @@
+"""This file contains variables used to connect to the database"""
+
+# Replace the following values with information from your database
+DATABASE_USER = "your_user_name"
+DATABASE_PASSWORD = "your_password"
+DATABASE_HOST = "localhost"
+DATABASE_PORT = "3306"
+DATABASE_NAME = "your_database_name"
