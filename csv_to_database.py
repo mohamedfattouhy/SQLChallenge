c7c6af4f7ca95e75e4db7282f4618d11423c2580
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2024-01-27 21:48:27 +0100

    Update csv_to_database file

--- csv_to_database.py
+++ csv_to_database.py
@@ -3,20 +3,19 @@
 
 import pandas as pd
 from sqlalchemy import create_engine
-
-
-# Replace the following values with information from your database
-DATABASE_USER = "your_user_name"
-DATABASE_PASSWORD = "your_password"
-DATABASE_HOST = "localhost"
-DATABASE_PORT = "3306"
-DATABASE_NAME = "your_database_name"
+from configuration.config import (
+    DATABASE_USER,
+    DATABASE_PASSWORD,
+    DATABASE_HOST,
+    DATABASE_PORT,
+    DATABASE_NAME,
+)
 
 # Create a database connection with SQLAlchemy (MySQL Server)
 engine = create_engine(
     f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 )
-conn = engine.connect()
+conn = engine.connect()  # Connection to the database
 
 # List of csv file names
 files = [
