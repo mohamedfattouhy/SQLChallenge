095b5c557f12313a5aeb9f83df5e69edb5df974a
mohamedfattouhy <78499945+mohamedfattouhy@users.noreply.github.com>
2024-01-27 21:54:34 +0100

    Add query 6

--- SQLSolutions.py
+++ SQLSolutions.py
@@ -72,3 +72,43 @@ query5 = conn.execute(
         limit 1;\
         ")
     )
+
+
+# ------------- Query 6 -------------
+# Delete duplicate records from work, product_size,
+# subject and image_link tables
+
+# Here's my solution for the table product_size. The same logic applies to the other tables.
+# Of course, you'll need to check for duplicates.
+# This can be done by checking whether the following number is
+# greater than 0 or not (only if the table has no primary key !):
+# select (select count(*) as cnt from table_name) - (select count(*) cnt_distinct from (select distinct * from table_name) x) as diff;
+query6 = conn.execute(
+    text(
+        "\
+         create table product_size_no_duplicate as\
+                \
+            select ps1.work_id, ps1.size_id, ps1.sale_price, ps1.regular_price\
+            from (select *, row_number() over (partition by work_id, size_id, sale_price, regular_price) as rn\
+            from product_size) as ps1\
+                \
+            join\
+                \
+            (select *, row_number() over (partition by work_id, size_id, sale_price, regular_price)  as rn\
+            from product_size) as ps2\
+                \
+            on (ps1.work_id = ps2.work_id\
+            and ps1.size_id = ps2.size_id\
+            and ps1.sale_price = ps2.sale_price\
+            and ps1.regular_price = ps2.regular_price\
+            and (ps1.rn=1 and ps2.rn=1));\
+                \
+        delete from product_size;\
+            \
+        insert into product_size\
+        select * from product_size_no_duplicate;\
+            \
+        drop table product_size_no_duplicate;\
+    "
+    )
+)
